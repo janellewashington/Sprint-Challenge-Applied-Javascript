1c4d5f1d61974b6d38c39c9c53c88dfae6078d39
janellewashington <janelle.s.washington@gmail.com>
2019-08-20 09:01:33 -0400

    MVP plus stretch attempt

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,40 @@
 class Carousel {
+    constructor(carouselElement) {
+        this.carouselElement = carouselElement;
+    
+        this.leftButton = this.carouselElement.querySelector(".left-button");
+        this.rightButton = this.carouselElement.querySelector(".right-button");
+    
+        this.images = this.carouselElement.querySelectorAll("img");
+    
+        this.index = 0;
+    
+        this.leftButton.addEventListener("click", () => this.nextPhotoLeft());
+        this.rightButton.addEventListener("click", () => this.nextPhotoRight());
+      }
+    
+      nextPhotoLeft() {
+        this.images.forEach(img => img.classList.remove("img-view"));
+        this.index--;
+        if (this.index < 0) {
+          this.index = 3;
+        }
+        this.images[this.index].classList.add("img-view");
+      }
+    
+      nextPhotoRight() {
+        this.images.forEach(img => img.classList.remove("img-view"));
+        this.index++;
+        if (this.index > 3) {
+          this.index = 0;
+        }
+        this.images[this.index].classList.add("img-view");
+      }
+    }
 
-}
-
-let carousel = document.querySelector();
+    let carousel = document
+    .querySelectorAll(".carousel")
+    .forEach(carousel => new Carousel(carousel));
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
